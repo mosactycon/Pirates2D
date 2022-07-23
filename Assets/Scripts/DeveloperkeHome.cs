a3013f21dab2d6284323cf1771dd288ea4f27a51
mosactycon <mighwaarfaaris89@gmail.com>
2022-07-23 10:12:14 +0700

    first commit

---
+++ Assets/Scripts/DeveloperkeHome.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeveloperkeHome : MonoBehaviour
+{
+    public void KeHome(){
+    	SceneManager.LoadScene("MainMenu");
+    }
+}
